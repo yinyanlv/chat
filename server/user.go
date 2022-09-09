cd75546f7526abce3ff72f25093cd9f34afb421f
bugong <1761869346@qq.com>
2022-09-10 00:00:17 +0800

    edit: user

--- server/user.go
+++ server/user.go
@@ -58,10 +58,40 @@ func (u *User) Offline() {
 
 	// 广播用户下线
 	u.server.Broadcast(u, "下线")
-
 }
 
-// 处理用户发送来的消息
+// 处理用户发送过来的消息
 func (u *User) HandleMessage(msg string) {
-	u.server.Broadcast(u, msg)
+	if msg == "query" {
+		// 查询所有在线用户
+		var res string
+		u.server.mapLock.Lock()
+		for _, user := range u.server.OnlineMap {
+			res += "[" + user.Address + "]" + user.Name + ":在线...\n"
+		}
+		u.server.mapLock.Unlock()
+		u.SendMessage(res)
+	} else if len(msg) > 5 && msg[:5] == "name|" {
+		newName := msg[5:]
+		u.server.mapLock.Lock()
+		_, ok := u.server.OnlineMap[newName]
+		// 当前用户名已存在
+		if ok {
+			u.SendMessage("当前用户名已被使用\n")
+			return
+		}
+		delete(u.server.OnlineMap, u.Name)
+		u.Name = newName
+		u.server.OnlineMap[newName] = u
+		u.server.mapLock.Unlock()
+
+		u.SendMessage("您已设置用户名为：" + u.Name + "\n")
+	} else {
+		u.server.Broadcast(u, msg)
+	}
+}
+
+// 向当前用户的客户端发送消息
+func (u *User) SendMessage(msg string) {
+	u.Conn.Write([]byte(msg))
 }
